0423f29db77d6e24f78c457115e3f4521f265bc1
Ayush1724 <ayushshrivastava2015@gmail.com>
2025-03-08 18:02:57 +0530

    Success

---
+++ routers/src/App.jsx
@@ -0,0 +1,20 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./home"
+import About from "./about"
+
+
+function App() {
+  
+
+  return (
+    <>
+    <Routes>
+      <Route path="/home" element={ <Home />}/>
+      <Route path="/about" element={ <About />}/>
+    </Routes>
+      
+    </>
+  )
+}
+
+export default App
